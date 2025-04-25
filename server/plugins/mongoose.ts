9852cbd0cc96a41f953a6efbed0133bc2bf12da2
linhtn238 <linhtn238@gmail.com>
2025-04-25 11:16:30 +0700

    fix lag medicine

--- server/plugins/mongoose.ts
+++ server/plugins/mongoose.ts
@@ -1,19 +1,25 @@
 import mongoose from "mongoose";
 
+let isConnected = false; // Cờ kiểm tra trạng thái kết nối
+
 export default defineNitroPlugin((nitroApp) => {
-  const config = useRuntimeConfig()
+  const config = useRuntimeConfig();
   const mongoUri = config.mongoUri || process.env.MONGO_URI;
-  
+
   if (!mongoUri) {
     console.error('MongoDB URI is not defined in environment variables or runtime config');
     return;
   }
-  
+
+  // Nếu đã kết nối rồi, không cần kết nối lại
+  if (isConnected) return;
+
   mongoose.connect(mongoUri as string, {})
     .then(() => {
-      console.log('Connected to MongoDB');
+      console.log('✅ Connected to MongoDB');
+      isConnected = true; // Đánh dấu đã kết nối thành công
     })
     .catch(err => {
-      console.error('Failed to connect to MongoDB', err);
+      console.error('❌ Failed to connect to MongoDB', err);
     });
 });
