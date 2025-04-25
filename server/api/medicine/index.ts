3b6acdeba93e648b618f2c4861aa641550684b2b
linhtn238 <linhtn238@gmail.com>
2025-04-25 13:03:36 +0700

    update code

--- server/api/medicine/index.ts
+++ server/api/medicine/index.ts
@@ -1,37 +1,31 @@
-import "@/server/models/Unit"
-import "@/server/models/Types"
-import "@/server/models/Category"
+import "@/server/models/Unit";
+import "@/server/models/Types";
+import "@/server/models/Category";
 import Medicine from "@/server/models/Medicine";
 
 export default defineEventHandler(async (event) => {
   const method = event.method;
 
   if (method === "GET") {
-    try {
-      const query = getQuery(event);
-      const page = parseInt(query.page as string) || 1;
-      const limit = parseInt(query.limit as string) || 10;
-      const skip = (page - 1) * limit;
-  
-      const [data, total] = await Promise.all([
-        Medicine.find()
-          .skip(skip)
-          .limit(limit)
-          .populate("unit_id category_id type_id"),
-        Medicine.countDocuments(),
-      ]);
-      
-  
-      return {
-        data,
-        total,
-        status: true,
-        message: "Get data successfully",
-      };
-    } catch (error) {
-      console.log("(❁´◡`❁)😒😒😒 ~ defineEventHandler ~ error:", error)
-      
-    }
+    const query = getQuery(event);
+    const page = parseInt(query.page as string) || 1;
+    const limit = parseInt(query.limit as string) || 10;
+    const skip = (page - 1) * limit;
+
+    const [data, total] = await Promise.all([
+      Medicine.find()
+        .skip(skip)
+        .limit(limit)
+        .populate("unit_id category_id type_id"),
+      Medicine.countDocuments(),
+    ]);
+
+    return {
+      data,
+      total,
+      status: true,
+      message: "Get data successfully",
+    };
   }
 
   if (method === "POST") {
